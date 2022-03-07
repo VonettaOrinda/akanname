1ad082af3a9ab6069351f67d0d85b314de5cdd12
Vonetta Orinda <you@example.com>
2022-03-07 11:13:58 +0300

    add function to get gender

--- js/script.js
+++ js/script.js
@@ -28,3 +28,41 @@ function getAkanName( ){
     }
 
 }
+//day validator
+function dayValidator(){
+    if (monthOfBirth ===2 && Number(yearofBirth)%4===0)
+        if (dayOfBirth > 28 || dayOfBirth < 1) {
+            return false;
+          } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+            return false;
+          } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+            return false;
+          } else {
+            return true;
+          }
+        } else if (dayOfBirth < 1 || dayOfBirth > 31){
+          return false;
+        } else {
+          return true;
+        }
+      }
+    
+      //validation variables
+      let monthValid = monthValidator();
+      let dayValid = dayValidator();
+    
+      //formula to determine day of birth (Sunday = 1, Monday = 2)etc..
+      let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
+              ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
+    
+      //creating arrays of Akan names for males and females
+      let maleAkanNames = [
+        "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame"
+      ];
+    
+      let femaleAkanNames = [
+        "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+      ];
+    
+    }
+}
